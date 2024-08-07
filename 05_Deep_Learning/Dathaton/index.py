c49bf0bbd6675f5b06aafccc0563e267444f4dc5
Eugenio G Diniz <leninediniz2710@gmail.com>
2024-08-06 22:40:56 -0300

    subindo

--- 05_Deep_Learning/Dathaton/index.py
+++ 05_Deep_Learning/Dathaton/index.py
@@ -138,13 +138,10 @@ elif paginaSelecionada == 'Aplicação':
         )
 
     def predict_chance(data):
-    # Criar um DataFrame a partir dos dados de entrada
+        # Criar um DataFrame a partir dos dados de entrada
         df = pd.DataFrame([data], columns=[
-            'INDE_2022', 'CG_2022', 'CF_2022', 'CT_2022', 'IAA_2022', 
-            'IEG_2022', 'IPS_2022', 'IDA_2022', 'NOTA_PORT_2022', 'NOTA_MAT_2022', 
-            'NOTA_ING_2022', 'QTD_AVAL_2022', 'IPP_2022', 'INDICADO_BOLSA_2022', 
-            'PONTO_VIRADA_2022', 'IPV_2022', 'IAN_2022', 'Ametista', 'Quartzo', 
-            'Topazio', 'Agata'
+        'IAA_2022', 'IEG_2022', 'IPS_2022', 'IDA_2022', 'NOTA_PORT_2022',
+        'NOTA_MAT_2022', 'NOTA_ING_2022', 'IPP_2022', 'INDICADO_BOLSA_2022'
         ])
         prediction = model.predict(df)
         return prediction[0]
@@ -156,10 +153,6 @@ elif paginaSelecionada == 'Aplicação':
     with st.form(key='student_form'):
         st.header('Informações do Aluno')
 
-    INDE_2022 = st.number_input('INDE_2022', format='%.2f')
-    CG_2022 = st.number_input('CG_2022', format='%.2f')
-    CF_2022 = st.number_input('CF_2022', format='%.2f')
-    CT_2022 = st.number_input('CT_2022', format='%.2f')
     IAA_2022 = st.number_input('IAA_2022', format='%.2f')
     IEG_2022 = st.number_input('IEG_2022', format='%.2f')
     IPS_2022 = st.number_input('IPS_2022', format='%.2f')
@@ -167,26 +160,13 @@ elif paginaSelecionada == 'Aplicação':
     NOTA_PORT_2022 = st.number_input('NOTA_PORT_2022', format='%.2f')
     NOTA_MAT_2022 = st.number_input('NOTA_MAT_2022', format='%.2f')
     NOTA_ING_2022 = st.number_input('NOTA_ING_2022', format='%.2f')
-    QTD_AVAL_2022 = st.number_input('QTD_AVAL_2022', format='%.2f')
     IPP_2022 = st.number_input('IPP_2022', format='%.2f')
-    INDICADO_BOLSA_2022 = st.number_input('INDICADO_BOLSA_2022', format='%.0f')
-    PONTO_VIRADA_2022 = st.number_input('PONTO_VIRADA_2022', format='%.0f')
-    IPV_2022 = st.number_input('IPV_2022', format='%.2f')
-    IAN_2022 = st.number_input('IAN_2022', format='%.2f')
-    Ametista = st.number_input('Ametista', format='%.0f')
-    Quartzo = st.number_input('Quartzo', format='%.0f')
-    Topazio = st.number_input('Topazio', format='%.0f')
-    Agata = st.number_input('Agata', format='%.0f')
-
+    
     submit_button = st.form_submit_button('Prever')
 
     # Exibir resultado
     if submit_button:
         input_data = {
-            'INDE_2022': INDE_2022,
-            'CG_2022': CG_2022,
-            'CF_2022': CF_2022,
-            'CT_2022': CT_2022,
             'IAA_2022': IAA_2022,
             'IEG_2022': IEG_2022,
             'IPS_2022': IPS_2022,
@@ -194,16 +174,7 @@ elif paginaSelecionada == 'Aplicação':
             'NOTA_PORT_2022': NOTA_PORT_2022,
             'NOTA_MAT_2022': NOTA_MAT_2022,
             'NOTA_ING_2022': NOTA_ING_2022,
-            'QTD_AVAL_2022': QTD_AVAL_2022,
-            'IPP_2022': IPP_2022,
-            'INDICADO_BOLSA_2022': INDICADO_BOLSA_2022,
-            'PONTO_VIRADA_2022': PONTO_VIRADA_2022,
-            'IPV_2022': IPV_2022,
-            'IAN_2022': IAN_2022,
-            'Ametista': Ametista,
-            'Quartzo': Quartzo,
-            'Topazio': Topazio,
-            'Agata': Agata
+            'IPP_2022': IPP_2022
         }
 
         result = predict_chance(input_data)
